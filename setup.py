3c204a3266ee561372886794492de29a4d4c1a85
= <walker@mfgis.com>
2019-01-23 11:23:13 -0800

    Bump version: 0.1.21 → 0.1.22

--- setup.py
+++ setup.py
@@ -50,6 +50,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/MiddleFork/rest_framework_factory',
-    version='0.1.21',
+    version='0.1.22',
     zip_safe=False,
 )
