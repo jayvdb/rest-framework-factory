8432f9bc7ff41193e50adb34b119ef477774b220
= <walker@mfgis.com>
2019-01-23 08:46:45 -0800

    modularize skel files

--- rest_framework_factory/factory.py
+++ rest_framework_factory/factory.py
@@ -6,6 +6,13 @@ skel_dir = os.path.join(os.path.dirname(__file__), 'skel')
 
 class Factory:
     def __init__(self):
+        self.skel = {}
+        for fn in os.listdir(skel_dir):
+            key = fn.split('.')[0]
+            with open(os.path.join(skel_dir, fn)) as fh:
+                self.skel[key] = fh.read()
+
+
         self.skel_file_model = os.path.join(skel_dir, 'models.py.txt')
         self.skel_file_api = os.path.join(skel_dir, 'api.py.txt')
 
@@ -15,24 +22,20 @@ class Factory:
 
     def create_from_scratch(self, model_name=None):
         """Create a new DRF API, model and all. """
-        model_name_lcase = model_name.lower()
-        with open(self.skel_file_model) as f:
-            content = f.read().format(model_name=model_name, model_name_lcase=model_name_lcase)
-        with open(self.skel_file_api) as f:
-            content += f.read().format(model_name=model_name, model_name_lcase=model_name_lcase)
+        skel_names = ['imports', 'models', 'serializer', 'filter', 'form', 'viewset', 'router','register']
+        content = self._generate_api_content(model_name=model_name, skel_names=skel_names)
         return content
 
     def build_from_model(self, app_name=None, model_name=None):
         """Build a DRF API from an existing django model
         The model name should be given as it appears in models.py ie it should be Upper case
         """
+        skel_names = ['serializer', 'filter', 'form', 'viewset', 'register']
         model = self._get_model_or_die(app_name, model_name)  # the model class, itself.
         # we know we have a valid model, for now all we do is build the api string.
-        content = self._generate_api_content(model_name=model_name)
+        content = self._generate_api_content(model_name=model_name, skel_names=skel_names)
         api_id = "{0}.{1}".format(app_name, model_name)
         self.apis[api_id] = content
-
-
 
     def build_from_app(self, app_name=None, model_list='__all__'):
         """Build DRF APIs for all or a subset of the models in a django app"""
@@ -49,8 +52,7 @@ class Factory:
                     raise ValueError("Model named {0} does not exist".format(m))
         for model in models:
             model_name = model._meta.object_name
-            model_name_lcase = model._meta.model_name
-            content = self.build_from_model()
+            self.build_from_model(app_name=app_name, model_name=model_name)
 
     def _get_app_or_die(self, app_name=None):
         """Return the app from django.apps.app_configs[app_name] or die trying"""
@@ -75,12 +77,21 @@ class Factory:
             print("Model named {0} not found in app {1}".format(model_name, app_name))
             raise
 
-    def _generate_api_content(self, model_name=None):
+    def _generate_api_content(self, model_name=None, skel_names=[]):
         if not model_name:
             raise ValueError("Model name required")
-        with open(self.skel_file_api) as f:
-            content = f.read().format(model_name=model_name, model_name_lcase=model_name.lower())
+        if not skel_names:
+            raise ValueError("List of skel files cannot be empty")
+        content = ''
+        for skel_name in skel_names:
+            try:
+                skel_file = os.path.join(skel_dir, skel_name + '.py.txt')
+                with open(skel_file) as f:
+                    content += f.read().format(model_name=model_name, model_name_lcase=model_name.lower())
+            except Exception:
+                print("Error reading skel file {0}".format(skel_file))
         return content
 
-
+    def _write_to_file(self):
+        pass
 
