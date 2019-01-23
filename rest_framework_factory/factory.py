901ebc53cb3dc04ac679cfa1c9aeda1609007c67
= <walker@mfgis.com>
2019-01-22 16:49:52 -0800

    beginning to take shape

--- rest_framework_factory/factory.py
+++ rest_framework_factory/factory.py
@@ -1,2 +1,26 @@
+import os
+skel_dir = os.path.join(os.getcwd(), 'skel')
+
 class Factory:
-    pass
+    def __init__(self):
+        self.apis = {}
+        self.apis['TestModel'] = self.create_from_scratch(model_name='TestModel')
+
+
+    def create_from_scratch(self, model_name=None):
+        """Create a new DRF API, model and all. """
+        model_name_lcase = model_name.lower()
+        skel_file = os.path.join(skel_dir, 'api.py.txt')
+        with open(skel_file) as f:
+            content = f.read().format(model_name=model_name, model_name_lcase=model_name_lcase)
+        return content
+
+
+
+        pass
+
+    def build_from_model(self, app_name=None, models=None):
+        """Build a DRF API from an existing django model"""
+
+
+
--- tests/test_rest_framework_factory.py
+++ tests/test_rest_framework_factory.py
@@ -35,3 +35,7 @@ def test_command_line_interface():
     help_result = runner.invoke(cli.main, ['--help'])
     assert help_result.exit_code == 0
     assert '--help  Show this message and exit.' in help_result.output
+
+def test_skeldir():
+    """Test the skeldir, ie the location of api template files"""
+    pass
