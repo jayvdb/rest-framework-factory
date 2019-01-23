4e84e4f85c187e2d5dc36fda883323f5a6041720
= <walker@mfgis.com>
2019-01-23 13:15:12 -0800

    model qualified name 11

--- rest_framework_factory/factory.py
+++ rest_framework_factory/factory.py
@@ -53,7 +53,7 @@ class Factory:
 
         content = ''
         #prliminary content
-        content += "import{0}.models\n".format(app_name)
+        content += "import {0}.models\n".format(app_name)
         skel_names = ['imports', 'router', ]
 
         for skel_name in skel_names:
