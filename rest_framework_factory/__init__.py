9be4b835e96e7ee18cf9b1be3b81f339b54437f3
= <walker@mfgis.com>
2019-01-22 16:56:25 -0800

    Bump version: 0.1.8 → 0.1.9

--- rest_framework_factory/__init__.py
+++ rest_framework_factory/__init__.py
@@ -4,4 +4,4 @@
 
 __author__ = """Steven Walker"""
 __email__ = 'walker@mfgis.com'
-__version__ = '0.1.8'
+__version__ = '0.1.9'
--- setup.py
+++ setup.py
@@ -50,6 +50,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/MiddleFork/rest_framework_factory',
-    version='0.1.8',
+    version='0.1.9',
     zip_safe=False,
 )
